e45efacd4e992104836f774fdfd762f74ee34a75
Raquel Moreira <rakelmoreiramendonca@gmail.com>
2022-02-09 21:02:58 -0300

    Componentes com TypeScript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,8 +3,14 @@ import { RepositoryItem } from './RepositoryItem';
 
 import '../styles/repositories.scss';
 
+interface Reposytory {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 export function RepositoryList() {
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Reposytory[]>([]);
 
     useEffect(() => {
         fetch('https://api.github.com/orgs/rocketseat/repos')
